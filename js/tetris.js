6d3392735d855f7b6cf169a90637de486444881d
TheTon1k <69811094+TheTon1k@users.noreply.github.com>
2021-03-10 01:17:41 +0300

    Update tetris.js

--- js/tetris.js
+++ js/tetris.js
@@ -565,7 +565,7 @@ function getGamePage(reload) {
             }
             let y2 = e.touches[0].clientY;
             let diffY = y1 - y2;
-            if (diffY > 150) {
+            if (diffY > 250) {
                 mainMenuSpan.click();
                 document.removeEventListener("touchmove", handleMoveToMainMenu);
                 cancelAnimationFrame(animation)
